7702df7bdc8714d5738ccc72eb031810d9dd14cf
Александр Мелодиев <alex@tessellastudio.com>
2018-07-27 17:56:39 +0500

    new

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const Session = require('telegraf/session')
 const stage = require('./scenes/scenes')
 const Queries = require('./helpers/queries')
 const Various = require('./helpers/various')
-
+const express = require('express')
+const expressApp = express();
 
 //Использование сессии для хранения данных
 bot.use(new Session())
@@ -28,8 +29,21 @@ bot.on('message', ctx=>{
     }
 })
 
-//запуск
-bot.startPolling()
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://uvcm.herokuapp.com';
+
+bot.telegram.setWebhook(`${URL}/bot`);
+expressApp.use(bot.webhookCallback(`/bot`));
+/*
+ your bot commands and all the other stuff on here ....
+*/
+// and at the end just start server on PORT
+expressApp.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+expressApp.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} adress ${URL}`);
+});
 
 
 //проверка свежих новостей
