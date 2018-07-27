e7995c8cadc32ca4f0a3ee1c7818487462644046
Александр Мелодиев <alex@tessellastudio.com>
2018-07-27 15:33:36 +0500

    sdf

--- bot.js
+++ bot.js
@@ -33,8 +33,8 @@ bot.on('message', ctx=>{
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://uvcm.herokuapp.com';
 
-bot.telegram.setWebhook(`${URL}/bot${token}`);
-bot.startWebhook(`/bot${token}, null, PORT`)
+bot.telegram.setWebhook(`${URL}/bot`);
+bot.startWebhook('/bot', null, PORT);
 
 
 //проверка свежих новостей
