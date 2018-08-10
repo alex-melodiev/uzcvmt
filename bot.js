26d6d59be9281de1d4f300d094c0ace8ef6a9a0a
Timur Kaiser <timurkayzer@gmail.com>
2018-08-10 10:23:20 +0500

    wrap in 'then'

--- bot.js
+++ bot.js
@@ -28,79 +28,80 @@ bot.on('message', ctx => {
 })
 
 
-/*
-*
-* */
-
 const PORT = process.env.PORT || 3000;
 const URL = 'https://uvcm.herokuapp.com';
 
 
 bot.telegram.setWebhook(`${URL}/bot`)
+    .then(res=>{
+        // Start https webhook
+        bot.startWebhook('/bot', null, PORT)
+
+        //проверка свежих новостей
+        setInterval(async () => {
+
+            //получаем новости
+            let news = await Queries.news.getNew()
+
+            //если они есть
+            if (!Various.isEmpty(news)) {
+
+                //получаем юзеров
+                let users = await Queries.user.getAll()
+
+                //отправляем каждую новость каждому юзеру
+                for (let i in news) {
+                    let messageText = ""
+
+                    if (news[i].title) messageText += '*' + news[i].title + '*\n'
+                    messageText += news[i].text
+
+
+                    for (let j in users) {
+                        try {
+                            if (news[i].image) {//если у новости есть изображение, посылаем его
+
+                                if (messageText) {
+                                    let res = await bot.telegram.sendPhoto(users[j].chatID, news[i].image, {
+                                        caption: messageText,
+                                        parse_mode: 'Markdown'
+                                    })
+                                }
+                                else {
+                                    let res = await bot.telegram.sendPhoto(users[j].chatID, news[i].image)
+                                }
+
+                            }
+                            else {//иначе посылаем только текст
+                                let res = await bot.telegram.sendMessage(users[j].chatID, messageText, {
+                                    parse_mode: 'Markdown'
+                                })
+
+                                console.log(res)
+                            }
+                        }
+                        catch (e) {
+                            console.log(e)
+                        }
+                    }
+
+
+                    //помечаем новость как прочитанную
+                    news[i].was_sent = '1'
+                    Queries.news.update(news[i].id, news[i])
+                }
+
+            }
+
+        }, 60000)
+    })
     .catch(e => {
         console.log(e)
     })
 
-// Start https webhook
-bot.startWebhook('/bot', null, PORT)
-
-//проверка свежих новостей
-setInterval(async () => {
-
-    //получаем новости
-    let news = await Queries.news.getNew()
-
-    //если они есть
-    if (!Various.isEmpty(news)) {
-
-        //получаем юзеров
-        let users = await Queries.user.getAll()
-
-        //отправляем каждую новость каждому юзеру
-        for (let i in news) {
-            let messageText = ""
-
-            if (news[i].title) messageText += '*' + news[i].title + '*\n'
-            messageText += news[i].text
 
 
-            for (let j in users) {
-                try {
-                    if (news[i].image) {//если у новости есть изображение, посылаем его
 
-                        if (messageText) {
-                            let res = await bot.telegram.sendPhoto(users[j].chatID, news[i].image, {
-                                caption: messageText,
-                                parse_mode: 'Markdown'
-                            })
-                        }
-                        else {
-                            let res = await bot.telegram.sendPhoto(users[j].chatID, news[i].image)
-                        }
-
-                    }
-                    else {//иначе посылаем только текст
-                        let res = await bot.telegram.sendMessage(users[j].chatID, messageText, {
-                            parse_mode: 'Markdown'
-                        })
-
-                        console.log(res)
-                    }
-                }
-                catch (e) {
-                    console.log(e)
-                }
-            }
-
-
-            //помечаем новость как прочитанную
-            news[i].was_sent = '1'
-            Queries.news.update(news[i].id, news[i])
-        }
-
-    }
-
-}, 60000)
 
 
 //отлов ошибок промисов
